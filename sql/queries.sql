b12510926bb6c54860f8de2cfa48af10fc3d59c3
Manav Mehta <mmanavmehta@gmail.com>
2023-10-06 20:52:29 +0530

    queries updation

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE team (
+CREATE TABLE organization (
     id SERIAL PRIMARY KEY,
-    parent_id SERIAL REFERENCES team(id) ON DELETE CASCADE,
+    parent_id SERIAL REFERENCES organization(id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
     path TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
@@ -13,30 +13,56 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    kotak_username VARCHAR(50),
     contact_num BIGINT NOT NULL,
     supervisor_id SERIAL REFERENCES users(id),
-    team_id SERIAL NOT NULL REFERENCES team(id),
+    organization_id SERIAL NOT NULL REFERENCES organization(id),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
----------------
+CREATE TYPE access_ENUM AS ENUM('R', 'W');
 
-INSERT INTO team (name, path) VALUES ('Kotak', '/');
-
-INSERT INTO team (name, parent_id) VALUES ('Risk', (SELECT id FROM team WHERE name = 'Kotak'));
-INSERT INTO team (name, parent_id) VALUES ('Treasury', (SELECT id FROM team WHERE name = 'Kotak'));
-INSERT INTO team (name, parent_id) VALUES ('Market Risk', (SELECT id FROM team WHERE name = 'Risk'));
-INSERT INTO team (name, parent_id) VALUES ('Credit Risk', (SELECT id FROM team WHERE name = 'Risk'));
-INSERT INTO team (name, parent_id) VALUES ('Counterparty Risk', (SELECT id FROM team WHERE name = 'Market Risk'));
-
-
-INSERT INTO users (name, email, contact_num, team_id) VALUES ('Sougat', 'sougat@chakraborty.com', 1234567890, 'fd75643b-7220-4e43-807a-1f4638448e55');
-INSERT INTO users (name, email, contact_num, team_id) VALUES ('Mohit', 'mohit@beniwal.com', 9876543210, '1fe7eb7b-95be-46b8-b3ce-74fa9748e26d');
-INSERT INTO users (name, email, contact_num, team_id) VALUES ('Manav', 'manav@mehta.com', 1111111111, '1fe7eb7b-95be-46b8-b3ce-74fa9748e26d');
-INSERT INTO users (name, email, contact_num, team_id) VALUES ('Piyush', 'piyush@sharma.com', 2222222222, '11a56ec9-fac9-4d59-9242-3f6945c20d4f');
+CREATE TABLE user_org_access (
+    id SERIAL PRIMARY KEY,
+    user_id SERIAL REFERENCES users(id),
+    organization_id SERIAL REFERENCES organization(id),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    access_type access_ENUM NOT NULL
+);
 
 ---------------
 
-SELECT * FROM team;
+INSERT INTO organization (name, path) VALUES ('Kotak', '/');
+
+INSERT INTO organization (name, parent_id, path) VALUES ('Risk', (SELECT id FROM organization WHERE name = 'Kotak'), '/Kotak/Risk');
+INSERT INTO organization (name, parent_id, path) VALUES ('Treasury', (SELECT id FROM organization WHERE name = 'Kotak'), '/Kotak/Treasury');
+INSERT INTO organization (name, parent_id, path) VALUES ('Market Risk', (SELECT id FROM organization WHERE name = 'Risk'), '/Kotak/Risk/MarketRisk');
+INSERT INTO organization (name, parent_id, path) VALUES ('Credit Risk', (SELECT id FROM organization WHERE name = 'Risk'), '/Kotak/Risk/CreditRisk');
+INSERT INTO organization (name, parent_id, path) VALUES ('Counterparty Risk', (SELECT id FROM organization WHERE name = 'Market Risk'), '/Kotak/Risk/MarketRisk/CounterpartyRisk');
+
+
+INSERT INTO users (name, email, contact_num, organization_id) VALUES ('Sougat', 'sougat@chakraborty.com', 1234567890, 1);
+INSERT INTO users (name, email, contact_num, organization_id) VALUES ('Mohit', 'mohit@beniwal.com', 9876543210, 2);
+INSERT INTO users (name, email, contact_num, organization_id) VALUES ('Manav', 'manav@mehta.com', 1111111111, 2);
+INSERT INTO users (name, email, contact_num, organization_id) VALUES ('Piyush', 'piyush@sharma.com', 2222222222, 3);
+
+
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Sougat'), (SELECT id FROM organization WHERE name = 'Risk'), 'W');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Sougat'), (SELECT id FROM organization WHERE name = 'Kotak'), 'R');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Mohit'), (SELECT id FROM organization WHERE name = 'Credit Risk'), 'W');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Manav'), (SELECT id FROM organization WHERE name = 'Market Risk'), 'W');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Mohit'), (SELECT id FROM organization WHERE name = 'Risk'), 'R');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Manav'), (SELECT id FROM organization WHERE name = 'Risk'), 'R');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Mohit'), (SELECT id FROM organization WHERE name = 'Treasury'), 'R');
+INSERT INTO user_org_access(user_id, organization_id, access_type) VALUES ((SELECT id FROM users WHERE name = 'Manav'), (SELECT id FROM organization WHERE name = 'Treasury'), 'R');
+
+---------------
+
+SELECT * FROM organization;
 SELECT * FROM users;
+
+-- ALTER TABLE organization DROP CONSTRAINT organization_parent_id_fkey;
+
+-- ALTER TABLE organization ADD CONSTRAINT organization_parent_id_fkey FOREIGN KEY (parent_id) REFERENCES organization (id) ON DELETE CASCADE;
